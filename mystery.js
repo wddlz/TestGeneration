3b62bc808b71488b4f0aba71b51a0ece150af620
Chris Parnin <chris.parnin@gmail.com>
2015-10-22 12:41:50 -0400

    mystery apperas

--- mystery.js
+++ mystery.js
@@ -1,22 +1,54 @@
 var fs = require("fs");
 
 function inc(p, q){
-    if(q ==undefined) q =1;
-
-   if( p < 30 )
+   if(q == 77 ) q =1;
+   if( p < -100 )
    {
    	p = -p;
    }
+   return p + q/q;
+}
 
-    return p + q/q;
+function weird(x,y,z, mode)
+{
+    if( x > 87 && y < 70 )
+    {
+        z = 33;
+    }    
+    else if( z < 42 )
+    {
+        if( mode == "strictly" )
+        {
+            return 0;
+        }
+        else if( mode.indexOf("bob") == 0 )
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        if( mode != "stricter" )
+        {
+            return y = z / x;
+        }
+    }
+    return 1;
 }
 
 
-function fileTest(dir, filePath)
+function fileTest(filePath, dir)
 {
 	if (!fs.existsSync(dir)){
-   	return false;
-	}
+
+   	    return false;
+    }
+    
+    var files = fs.readdirSync(dir);
+    if( files.length == 0 )
+    {
+        return false;
+    }
 
    if( fs.existsSync(filePath ))
    {
@@ -41,7 +73,7 @@ function normalize(phoneNumber) {
 function format(phoneNumber, formatString, options) 
 {
     // Normalize the phone number first unless not asked to do so in the options
-    if (!options || !options.shouldNormalize) {
+    if (!options || !options.normalize) {
       phoneNumber = normalize(phoneNumber)
     };
 
@@ -57,7 +89,7 @@ function blackListNumber(phoneNumber)
 {
 	var num = format(phoneNumber, "(NNN) NNN-NNNN");
 	var area = num.substring(1,4);
-	if( area == "919" )
+	if( area != "919" )
 	{
 		return true;
 	}
@@ -68,5 +100,6 @@ exports.fileTest = fileTest;
 exports.normalize = normalize;
 exports.format = format;
 exports.inc = inc;
+exports.weird = weird;
 exports.blackListNumber = blackListNumber;
 
